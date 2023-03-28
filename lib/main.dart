afc91d337e4108afefdfe6fed4b5dd89b7e0f28e
aquabuz <aquabuz@gmail.com>
2023-03-28 22:19:07 +0900

    😶‍🌫️ Pomodoro App

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool show = true;
-
-  void toggle() {
-    print('toggle');
-    setState(() {
-      show = !show;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.amberAccent,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
-      ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              show ? const NewWidget() : const Text('not'),
-              IconButton(
-                onPressed: toggle,
-                icon: const Icon(
-                  Icons.ac_unit,
-                ),
-              ),
-            ],
-          ),
+        cardColor: const Color(0xFFF4EDDB),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
         ),
       ),
-    );
-  }
-}
-
-class NewWidget extends StatefulWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override
-  State<NewWidget> createState() => _NewWidgetState();
-}
-
-class _NewWidgetState extends State<NewWidget> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    print('init');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build');
-    return Text(
-      'Click Count',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
