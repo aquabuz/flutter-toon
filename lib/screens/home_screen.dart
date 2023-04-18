e1fa4e2f3e2737c6b5829596f2b219a933f99bea
aquabuz <aquabuz@gmail.com>
2023-04-18 23:35:34 +0900

    Merge branch 'feature/toon' into release

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const numb = 1500;
-  int totalSeconds = numb;
-  int totalPomodoros = 0;
-  late Timer timer;
-  bool isRunning = false;
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split('.')[0].substring(2);
-  }
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros = totalPomodoros + 1;
-        isRunning = false;
-        totalSeconds = numb;
-      });
-      return timer.cancel();
-    }
-    setState(() {
-      totalSeconds = totalSeconds - 1;
-    });
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-=======
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/service/api_service.dart';
@@ -62,84 +7,10 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
->>>>>>> feature/toon
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 90,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Center(
-              child: IconButton(
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(
-                  isRunning
-                      ? Icons.pause_circle_filled_outlined
-                      : Icons.play_circle_outlined,
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                      borderRadius: BorderRadius.circular(60),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodoros',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                          ),
-                        ),
-                        Text(
-                          '$totalPomodoros',
-                          style: TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.w600,
-                            color:
-                                Theme.of(context).textTheme.displayLarge!.color,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-=======
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
@@ -183,7 +54,6 @@ class HomeScreen extends StatelessWidget {
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 20,
->>>>>>> feature/toon
       ),
     );
   }
